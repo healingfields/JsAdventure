b6f3fa1fbd3733dbf85a83680e6970398617b5dd
healingfields <dihratene@gmail.com>
2022-02-06 13:37:47 +0100

    configuration of the projects(work usually done by create react app)

--- reactBarBones/adopt-me/src/app.js
+++ reactBarBones/adopt-me/src/app.js
@@ -1,17 +1,26 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, props.name),
-      React.createElement("h1", {}, props.animal),
-      React.createElement("h1", {}, props.breed),
-    ]);
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./pet";
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "my-brand" }, "adopt me!"),
+//     ...[1, 2, 3, 4].map((i) => React.createElement("h2", {}, i)),
+//     React.createElement(Pet, { name: "luna", animal: "dog", breed: "bulldog" }),
+//     React.createElement(Pet, { name: "dolly", animal: "dog", breed: "terrier" }),
+//     React.createElement(Pet, { name: "picha", animal: "dog", breed: "rotweiler" }),
+//   ]);
+// };
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "adopt me!"),
-    ...[1, 2, 3, 4].map((i) => React.createElement("h2", {}, i)),
-    React.createElement(Pet, { name: "luna", animal: "dog", breed: "bulldog" }),
-    React.createElement(Pet, { name: "dolly", animal: "dog", breed: "terrier" }),
-    React.createElement(Pet, { name: "picha", animal: "dog", breed: "rotweiler" }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt me</h1>
+      <Pet name="luna" animal="dog" breed="bully" />
+      <Pet name="dolly" animal="dog" breed="golden retriever" />
+      <Pet name="picha" animal="dog" breed="rotweiler" />
+    </div>
+  );
 };
+
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
