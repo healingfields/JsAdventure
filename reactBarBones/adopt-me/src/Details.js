51def7117482a674a74c1cebba6a8dc501bb7b0a
healingfields <dihratene@gmail.com>
2022-02-08 23:34:02 +0100

    added a carousel component

--- reactBarBones/adopt-me/src/Details.js
+++ reactBarBones/adopt-me/src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   //   constructor() {
@@ -19,9 +20,11 @@ class Details extends Component {
     if (this.state.loading) {
       return <h2>... Loading</h2>;
     }
-    const { animal, breed, name, city, description, state } = this.state;
+    const { animal, breed, name, city, description, state, images } =
+      this.state;
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
