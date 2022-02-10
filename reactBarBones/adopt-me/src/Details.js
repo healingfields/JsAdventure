d7c433082c49cf1d275687ead2d72c4899c46756
healingfields <dihratene@gmail.com>
2022-02-10 12:46:31 +0100

    creating  a modal and implementing it

--- reactBarBones/adopt-me/src/Details.js
+++ reactBarBones/adopt-me/src/Details.js
@@ -3,13 +3,14 @@ import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends Component {
   //   constructor() {
   //     super();
   //     this.state = { loading: true };
   //   }
-  state = { loading: true };
+  state = { loading: true, showModal: false };
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
@@ -18,6 +19,8 @@ class Details extends Component {
     this.setState(Object.assign({ loading: false }, json.pets[0]));
     console.log(this.state);
   }
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  adopt = () => (window.location = "http://bit.ly/pet-adopt");
   render() {
     if (this.state.loading) {
       return <h2>... Loading</h2>;
@@ -30,6 +33,7 @@ class Details extends Component {
       description,
       state,
       images,
+      showModal,
     } = this.state;
 
     return (
@@ -42,11 +46,27 @@ class Details extends Component {
           </h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}> Adopt me</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                {" "}
+                Adopt me
+              </button>
             )}
           </ThemeContext.Consumer>
-
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}</h1>
+              </div>
+              <div className="buttons">
+                <button onClick={this.adopt}>Adopt</button>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
