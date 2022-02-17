2a921c2ae66b7b5c696f311047f52e2755ad41f2
healingfields <dihratene@gmail.com>
2022-02-17 16:10:59 +0100

    code splitting

--- reactBarBones/adopt-me/src/app.js
+++ reactBarBones/adopt-me/src/app.js
@@ -1,9 +1,12 @@
-import React, { StrictMode, useState } from "react";
+import React, { StrictMode, useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Details from "./Details";
-import SearchPrams from "./SearchParams";
+// import Details from "./Details";
+// import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 // const App = () => {
 //   return React.createElement("div", {}, [
@@ -19,31 +22,33 @@ const App = () => {
   const theme = useState("darkblue");
   return (
     <ThemeContext.Provider value={theme}>
-      <div
-        className="p-0 m-0"
-        style={{
-          background:
-            "url(https://pets-images.dev-apis.com/pets/wallpaperC.jpg) ",
-        }}
-      >
-        <Router>
-          <header className="w-full text-center bg-gradient-to-b from-purple-500 to-blue-500 p-6 mb-9">
-            <Link to="/">
-              <h1 className="text-3xl text-white font-sans hover:text-gray-400">
-                Adopt me!
-              </h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchPrams />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      <Suspense fallback={<h2>loading route ...</h2>}>
+        <div
+          className="p-0 m-0"
+          style={{
+            background:
+              "url(https://pets-images.dev-apis.com/pets/wallpaperC.jpg) ",
+          }}
+        >
+          <Router>
+            <header className="w-full text-center bg-gradient-to-b from-purple-500 to-blue-500 p-6 mb-9">
+              <Link to="/">
+                <h1 className="text-3xl text-white font-sans hover:text-gray-400">
+                  Adopt me!
+                </h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </Suspense>
     </ThemeContext.Provider>
   );
 };
