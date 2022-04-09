5a62de4a16998922147568d55b05e8b5d6a87b11
healingfields <dihratene@gmail.com>
2022-04-09 14:20:14 +0000

    last updates to the vertical slider project

--- JSProjects/dbl-vertical-slider/script.js
+++ JSProjects/dbl-vertical-slider/script.js
@@ -4,10 +4,11 @@ const down = document.getElementById('down')
 const up = document.getElementById('up')
 const slidesLength = right.querySelectorAll('div').length
 const sliderContainer = document.querySelector('.slider-container')
-
+console.log(slidesLength);
 let activeSlideIndex = 0
 
-left.style.top = `-${(slidesLength - 1) * 100}vh`
+// left.style.top = `-${(slidesLength - 1) * 100}vh`
+console.log(`-${(slidesLength - 1) * 100}vh`);
 
 up.addEventListener('click', () => changeSlide('up'))
 down.addEventListener('click', () => changeSlide('down'))
@@ -17,11 +18,13 @@ const changeSlide = (direction) => {
     const sliderHeight = sliderContainer.clientHeight
  
     if(direction === 'up') {
+        console.log('up');
         activeSlideIndex++
-        if(activeSlideIndex >= slidesLength - 1) {
+        if(activeSlideIndex > slidesLength - 1) {
             activeSlideIndex = 0
         }
     } else if(direction === 'down') {
+        console.log('down');
         activeSlideIndex--
         if(activeSlideIndex < 0) {
             activeSlideIndex = slidesLength - 1
@@ -29,6 +32,6 @@ const changeSlide = (direction) => {
     }
 
     right.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
-    left.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+    left.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
 }
 
