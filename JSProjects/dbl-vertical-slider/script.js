00f29dd7f3965d327181d9dff2b0761cb8bbc63a
healingfields <dihratene@gmail.com>
2022-04-09 04:00:41 +0000

    added script

---
+++ JSProjects/dbl-vertical-slider/script.js
@@ -0,0 +1,34 @@
+const left = document.querySelector('.left-side')
+const right = document.querySelector('.right-side')
+const down = document.getElementById('down')
+const up = document.getElementById('up')
+const slidesLength = right.querySelectorAll('div').length
+const sliderContainer = document.querySelector('.slider-container')
+
+let activeSlideIndex = 0
+
+left.style.top = `-${(slidesLength - 1) * 100}vh`
+
+up.addEventListener('click', () => changeSlide('up'))
+down.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    
+    const sliderHeight = sliderContainer.clientHeight
+ 
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex >= slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+
+    right.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    left.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
+
