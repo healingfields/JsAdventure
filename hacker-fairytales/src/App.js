044e0e23af965e628a1b36032893ef3455bcaa61
healingfields <dihratene@gmail.com>
2022-04-16 16:19:28 +0000

    error and loading handling

--- hacker-fairytales/src/App.js
+++ hacker-fairytales/src/App.js
@@ -87,8 +87,9 @@ const App = () => {
   ]
 
   const [searchTerm, setSearchTerm] = useSemiPersistentHook('search', 'react')
-
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false)
 
   const getAsyncStories = () =>
     new Promise(resolve => 
@@ -100,9 +101,11 @@ const App = () => {
 
 
   React.useEffect(()=>{
+    setIsLoading(true)
     getAsyncStories().then(result => {
       setStories(result.data.stories);
-    });
+      setIsLoading(false)
+    }).catch(()=>setIsError(true));
   }, []);
 
   const handleSearch = (event) =>{
@@ -133,7 +136,9 @@ const App = () => {
        <strong>Search2 :</strong> 
       </InputWithLabel> */}
       <hr/>
-       <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong ....</p>}
+      {isLoading ? (<p>Loading....</p>) : (<List list={searchedStories} onRemoveItem={handleRemoveStory} />)}
+       
     </div>
   );
 }
